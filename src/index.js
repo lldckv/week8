511db34ec1e00b30b87ad090b8afc83f9a79b65b
lldckv <lldckv@gmail.com>
2026-06-15 17:55:22 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
+  if (req.path.length > 1 && !req.path.endsWith('/')) {
+    const query = req.url.slice(req.path.length);
+    return res.redirect(308, req.path + '/' + query);
+  }
+  next();
+});
+
+const userSchema = new mongoose.Schema({
+  login: String,
+  password: String
+});
+
+app.post('/insert/', async (req, res) => {
+  const login = req.body.login;
+  const password = req.body.password;
+  const url = req.body.URL || req.body.url;
+
+  let connection;
+  try {
+    connection = await mongoose.createConnection(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    const User = connection.model('users', userSchema, 'users');
+    await User.create({ login, password });
+
+    res.type('text/plain').send('OK');
+  } catch (e) {
+    res.status(500).type('text/plain').send(String(e));
+  } finally {
+    if (connection) {
+      await connection.close();
+    }
+  }
+});
+
+app.listen(process.env.PORT);
